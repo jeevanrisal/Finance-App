e224fbf8103068856cec55fd963de32fd9be45fb
JRisal01 <jrisal74@gmail.com>
2025-03-15 21:33:04 +1100

    Initial Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000')
+      .then((response) => setMessage(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return <h1>{message}</h1>;
+}
+
+export default App;
